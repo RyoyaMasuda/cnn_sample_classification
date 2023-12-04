855fa5b8cb4a03572779b7349fe1ef3bd21715a4
RyoyaMasuda <94744317+RyoyaMasuda@users.noreply.github.com>
2023-12-04 22:27:35 +0900

    Update app.py
    エラー修正

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from model import predict
 
 # 非推奨の処理に対する警告を非表示にする
 # st.set_option("deprecation.showfileUploaderEncoding", False)
-st.set_option("deprecation.file_uploader.encoding", False)
+# st.set_option("deprecation.file_uploader.encoding", False)
 
 st.sidebar.title('画像認識アプリ(sample)')
 st.sidebar.write('オリジナルの画像認識モデルを使って画像を判定します。')
